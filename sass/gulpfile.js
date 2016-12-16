9c67ee14de551184eabc2d113746cbbf7fb0b259
Raúl Bojo <bojo.raul@gmail.com>
2016-12-16 23:44:54 +0100

    L1.6 - Nesting selectors

--- sass/gulpfile.js
+++ sass/gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 
 var sass = require('gulp-sass');
 gulp.task('sass', function () {
-  gulp.src('./scss/**/*.scss')
+  gulp.src('./scss/**.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
